05e2e0f1e6d6daf7ef7334dc73ed5a85c9d90988
vstegen <github.pQcVWa7e@papertrail.fyi>
2025-10-24 23:30:29 +0900

    add skeleton for clean command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(name = "photo-cleanup")]
+#[clap(arg_required_else_help = true)]
+#[clap(version)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Clean {
+        #[clap(short, long)]
+        raw: PathBuf,
+        #[clap(short, long)]
+        compressed: PathBuf,
+        #[clap(long)]
+        dry: Option<bool>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Clean {
+            raw,
+            compressed,
+            dry: _,
+        } => {
+            println!(
+                "raw path: {}, compressed path: {}",
+                raw.display(),
+                compressed.display()
+            );
+        }
+    }
 }
